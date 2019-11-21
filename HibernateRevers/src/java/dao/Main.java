e371f418e098247605fb2da86e08561dc59df654
khalid7487 <khalid15-7487@diu.edu.bd>
2019-11-21 19:55:01 +0600

    hibernateUtil

---
+++ HibernateRevers/src/java/dao/Main.java
@@ -0,0 +1,17 @@
+
+package dao;
+
+import entity.User;
+
+/**
+ *
+ * @author KHALID
+ */
+public class Main {
+    public static void main(String[] args) {
+        User us=new User();
+        us.setUser("MD khalid Hossen");
+        us.setPassword("7487");
+    }
+    
+}
